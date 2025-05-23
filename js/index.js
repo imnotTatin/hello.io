8f5e3307a9d2dc4fe86aaee86058a9ee849569b2
imnotTatin <wasabeyu@gmail.com>
2025-05-23 18:22:20 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hi, baby kuh🥹'.split('')
+const text = 'Hi, baby kuh🥰'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
